c20548c27a85244c7699f6e1248685e18723f7b0
Vishakha Bhilwadkar <vishakhabhilwadkar@gmail.com>
2025-07-08 11:11:56 +0530

    Day 6 code added

---
+++ src/Division.java
@@ -0,0 +1,10 @@
+
+public class Division {
+	public static void main(String[] args) {
+		int a =32;
+		int b = 4;
+        int result = a / b;
+        System.out.println("Division: " + result);
+
+	}
+}
---
+++ src/Multiplication.java
@@ -0,0 +1,13 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+	
+				int a = 30;
+				int b = 4;
+		        int mult = a * b;
+		        System.out.println("Multiplication: " + mult);
+	
+	}
+
+}
